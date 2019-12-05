8dc84afa46713de4b228f2bfdf69caf4d0212ea5
Rugal Bernstein <rugal.bernstein@thomsonreuters.com>
2019-12-05 16:02:01 -0500

    Inject TrackerService into Tracker

--- src/main/java/config/TrackerContext.java
+++ src/main/java/config/TrackerContext.java
@@ -4,30 +4,45 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import ga.rugal.pt.core.entity.Post;
 import ga.rugal.pt.core.service.PostService;
 
 import com.turn.ttorrent.tracker.TrackedTorrent;
 import com.turn.ttorrent.tracker.Tracker;
+import com.turn.ttorrent.tracker.TrackerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Torrent tracker application context.
  *
  * @author rugal
  */
+@Configuration
 @Slf4j
 public class TrackerContext {
+
+  @Bean
+  public ConcurrentMap<String, TrackedTorrent> torrents() {
+    return new ConcurrentHashMap<>();
+  }
+
+  @Bean
+  public TrackerService service(final ConcurrentMap<String, TrackedTorrent> torrents) {
+    return new TrackerService(torrents);
+  }
 
   /**
    * Create a tracker server in local, with same port to servlet container Spring will start this
    * tracker after creation of bean.<BR>
    * Load all valid torrent from database and announce them in tracker, then start tracker thread.
    *
-   * @param postService
+   * @param postService PostService that get all posts
+   * @param service     TrackerService that actually handle the request
    *
    * @return
    *
@@ -36,10 +51,10 @@ public class TrackerContext {
    *
    */
   @Bean(initMethod = "start", destroyMethod = "stop")
-  @Scope("singleton")
-  public Tracker tracker(final PostService postService) throws IOException,
-                                                               NoSuchAlgorithmException {
-    final Tracker tracker = new Tracker(InetAddress.getLocalHost());
+  public Tracker tracker(final PostService postService, final TrackerService service)
+          throws IOException, NoSuchAlgorithmException {
+
+    final Tracker tracker = new Tracker(InetAddress.getLocalHost(), service);
     for (Post p : postService.getDao().findAll()) {
       if (Objects.nonNull(p.getBencode())) {
         tracker.announce(new TrackedTorrent(p.getBencode()));
