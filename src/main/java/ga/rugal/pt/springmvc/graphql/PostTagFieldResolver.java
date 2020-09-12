f961b092a2a188c542428bd4616c24c27256eb2c
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2020-09-12 00:31:11 -0400

    Resolve fields of PostTag

--- src/main/java/ga/rugal/pt/springmvc/graphql/PostTagFieldResolver.java
+++ src/main/java/ga/rugal/pt/springmvc/graphql/PostTagFieldResolver.java
@@ -4,8 +4,12 @@ import ga.rugal.glavenus.graphql.PostDto;
 import ga.rugal.glavenus.graphql.PostTagDto;
 import ga.rugal.glavenus.graphql.PostTagResolver;
 import ga.rugal.glavenus.graphql.TagDto;
+import ga.rugal.pt.core.service.PostTagService;
+import ga.rugal.pt.springmvc.mapper.PostMapper;
+import ga.rugal.pt.springmvc.mapper.TagMapper;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -16,13 +20,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class PostTagFieldResolver implements GraphQLResolver<PostTagDto>, PostTagResolver {
 
+  @Autowired
+  private PostTagService postTagService;
+
   @Override
   public PostDto post(final PostTagDto postTagDto) throws Exception {
-    throw new UnsupportedOperationException("Not supported yet.");
+    return PostMapper.I.from(this.postTagService.getDao()
+      .findById(postTagDto.getPtid()).get().getPost());
   }
 
   @Override
   public TagDto tag(final PostTagDto postTagDto) throws Exception {
-    throw new UnsupportedOperationException("Not supported yet.");
+    return TagMapper.I.from(this.postTagService.getDao()
+      .findById(postTagDto.getPtid()).get().getTag());
   }
 }
